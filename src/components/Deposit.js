6b7e95a7f6e5fd3c893a17591bedb766fb011320
Vida <herling.codingcamp@gmail.com>
2022-06-25 01:00:29 -0400

    add deposit functionality

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Card, Form, Button } from "react-bootstrap";
 
 export default function Deposit() {
+  const [amount, setAmount] = React.useState(0);
+  const [balance, setBalance] = React.useState(0);
+
+  // const validateNum = (value) => {
+  //   if(value < 0) {
+  //     return message = "error";
+  //   }
+  // }
+
+  const handleDeposit = () => {
+    if(+amount > 0) {
+      setBalance(+balance + +amount);
+      console.log("balance: " + balance + typeof(balance));
+      console.log("amount: " + amount + typeof(amount))
+    } else return;
+  }
+
+  const buttonDisabled = () => {
+    if(isNaN(amount) || amount < 0) return true;
+    else return false;
+  }
+
   return (
-    <h1>Deposit</h1>
+    <>
+      <Card style={{ width: '35rem' }}>
+        <Card.Header>DEPOSIT</Card.Header>
+        <Card.Body>
+          <Card.Title>Balance: {balance}</Card.Title>
+          <Form.Group className="mb-3" controlId="deposit">
+            <Form.Label>Deposit Amount</Form.Label>
+            <Form.Control type="text" value={amount} onChange={e => setAmount(e.target.value)} />
+            {(isNaN(amount) || amount < 0) && <Form.Text>Error: must be a positive number</Form.Text>} 
+          </Form.Group>
+          <Button variant="success" onClick={handleDeposit} disabled={buttonDisabled()}>DEPOSIT</Button>
+        </Card.Body>
+      </Card>
+    </>
+
   )
 };
