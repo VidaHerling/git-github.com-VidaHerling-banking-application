0c25f7278b1094ba6ef0d42f4b4d41de720a9157
Vida <herling.codingcamp@gmail.com>
2022-06-25 21:58:06 -0400

    success message

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -2,25 +2,21 @@ import React from 'react';
 import { Card, Form, Button } from "react-bootstrap";
 
 export default function Deposit() {
-  const [amount, setAmount] = React.useState(0);
+  const [deposit, setDeposit] = React.useState("");
   const [balance, setBalance] = React.useState(0);
 
-  // const validateNum = (value) => {
-  //   if(value < 0) {
-  //     return message = "error";
-  //   }
-  // }
-
   const handleDeposit = () => {
-    if(+amount > 0) {
-      setBalance(+balance + +amount);
+    if(+deposit > 0) {
+      setBalance(+balance + +deposit);
+      setTimeout(() => alert(`You have successfully deposited $ ${deposit}!`), 250);
+      setDeposit("");
       console.log("balance: " + balance + typeof(balance));
-      console.log("amount: " + amount + typeof(amount))
+      console.log("deposit: " + deposit + typeof(deposit))
     } else return;
   }
 
   const buttonDisabled = () => {
-    if(isNaN(amount) || amount < 0) return true;
+    if(isNaN(deposit) || deposit <= 0) return true;
     else return false;
   }
 
@@ -29,11 +25,11 @@ export default function Deposit() {
       <Card style={{ width: '35rem' }}>
         <Card.Header>DEPOSIT</Card.Header>
         <Card.Body>
-          <Card.Title>Balance: {balance}</Card.Title>
+          <Card.Title>Balance: $ {balance}</Card.Title>
           <Form.Group className="mb-3" controlId="deposit">
-            <Form.Label>Deposit Amount</Form.Label>
-            <Form.Control type="text" value={amount} onChange={e => setAmount(e.target.value)} />
-            {(isNaN(amount) || amount < 0) && <Form.Text>Error: must be a positive number</Form.Text>} 
+            <Form.Label>Amount deposit</Form.Label>
+            <Form.Control type="text" value={deposit} onChange={e => setDeposit(e.target.value)} />
+            {(isNaN(deposit) || deposit < 0) && <Form.Text>Error: the amount must be a positive number</Form.Text>}
           </Form.Group>
           <Button variant="success" onClick={handleDeposit} disabled={buttonDisabled()}>DEPOSIT</Button>
         </Card.Body>
