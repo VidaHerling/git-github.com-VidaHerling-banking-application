3b6bc32c13356f56f6d1c503fa0859946d4c3b20
Vida <herling.codingcamp@gmail.com>
2022-06-27 17:48:47 -0400

    update validation message

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -8,10 +8,30 @@ export default function CreateAccount() {
   const [name, setName]         = React.useState("");
   const [email, setEmail]       = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [balance, setBalance] = React.useState("");
   let users = React.useContext(userContext);
 
+  const validate = (field, label) => {
+    if (!field) {
+      setStatus(' Error: ' + label + " can't be blank");
+      setTimeout(() => setStatus(''),3000);
+      return false;
+    }
+    return true;
+}
+
   const handleCreate = () => {
-    users.push({name, email, password});
+    if(!validate(name, "name")) return;
+    if(!validate(email, "email")) return;
+    if(password.length < 8) {
+      setStatus(" Error: password must be at least 8 characters long");
+      return status;
+    }
+    if(balance < 200) {
+      setStatus(" Error: you must deposit at least $200 to create an account")
+      return status;
+    }
+    users.push({name, email, password, balance});
     setShow(false);
     alert(`Your account is successfully created!`);
  }
@@ -22,12 +42,13 @@ export default function CreateAccount() {
     setName("");
     setEmail("");
     setPassword("");
+    setBalance("");
     setShow(true);
   }
 
-  //the create account button is disabled if the form is imcomplete (password must be at least 8 characters long)
+  //the create account button is disabled if the form is blank
   const buttonDisabled = () => {
-    if(!name || !email || password.length < 8) return true;
+    if(!name && !email && !password && !balance) return true;
     else return false;
   }
 
@@ -46,12 +67,17 @@ export default function CreateAccount() {
             <Form.Control type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="password">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>Password</Form.Label><br/>
             <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="balance">
+            <Form.Label>Initial deposit</Form.Label><br/>
+            <Form.Text>You must deposit at least $200 to create an account</Form.Text>
+            <Form.Control type="number" placeholder="Intial Balance" value={balance} onChange={e => setBalance(e.target.value)}/>
           </Form.Group>
           </Form>
           <Button variant="success" onClick={handleCreate} disabled={buttonDisabled()}>Create Account</Button>
-          {status}
+          <div className="error-message">{status}</div>
         </Card.Body>) : 
         (<Button variant="primary" onClick={clearForm}>Create Another Account</Button>)
         }
