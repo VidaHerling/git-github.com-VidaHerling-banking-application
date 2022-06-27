e0097d7a39e1fc16608061fc35b66cabc2541f4f
Vida <herling.codingcamp@gmail.com>
2022-06-27 17:48:34 -0400

    add mouseHover effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import "./components.css";
 
-export default function NavBar() {
+export default function NavBar() {  
+  const [hover, setHover] = React.useState(false);
+
+  const onHover = () => {
+    setHover(true);
+  }
+
+  const onLeave = () => {
+    setHover(false);
+  }
+
+  const services = ['Create Account', 'Deposit', 'Withdraw', 'All Data'];
+
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -10,18 +23,22 @@ export default function NavBar() {
       <Link className="navbar-brand" to="/">Steadfast Bank</Link>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/CreateAccount/">Create Account</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/deposit/">Deposit</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/withdraw/">Withdraw</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/alldata/">AllData</Link>
-          </li>          
+          {services.map((service) => (
+            <li className="nav-item">
+              <OverlayTrigger
+                placement="bottom"
+                overlay={
+                  <Tooltip>
+                    {service}
+                  </Tooltip>
+                }>
+                <Link className="nav-link" to={service.replace(/\s+/g, '')} onMouseEnter={onHover} onMouseLeave={onLeave}>
+                  {service}
+                </Link>
+              </OverlayTrigger>
+            </li>
+          ))
+          }
         </ul>
       </div>
     </nav>
